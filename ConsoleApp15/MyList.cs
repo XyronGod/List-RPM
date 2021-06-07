16c137bfacbf7c3ada5a9a5cdfacf23e57143ffa
John Doe <johndoe@example.com>
2021-06-07 16:05:53 +0500

    first commit

---
+++ ConsoleApp15/MyList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp15
+{
+	class MyList<T>
+	{
+		public void Add (T value)
+		{
+		     Node<T> name = new Node<T>();
+		     name.value = value;
+			 if ( firstElement == null)
+			 {
+				firstElement = name;
+			 }
+			 else
+			 {
+				GetLast().next = name;
+			 }
+		}
+		Node<T> GetLast()
+		{
+			Node<T> last = firstElement;
+			while (last.next != null)
+				last = last.next;
+			return last;
+		}
+		Node<T> firstElement;
+		public void Print()
+		{
+			Node<T> last = firstElement;
+			while(last != null)
+			{
+				Console.WriteLine(last.value);
+				last = last.next;
+			}
+		}
+	}
+	
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyList<string> list = new MyList<string>();
+			list.Add("z");
+			list.Add("x");
+			list.Add("Тычка");
+			list.Add("Пауза");
+			list.Add("?????");
+			list.Add("анпауз");
+			list.Add("c");
+			list.Print();
+		}
+	}
+}
+
